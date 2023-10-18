7baa3b3feee66cd455940a23ea7b7b9649a5f0b1
Nilesh-Patil0310 <patilnilesh093@gmail.com>
2023-10-18 18:45:14 +0530

    update readme file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Social_media-WebApp
+- Create Folder with Project Name.
+- Run npm init to initiate the Project.
+- Create the required directories for project.
+- 
---
+++ app.js
@@ -0,0 +1,17 @@
+// import express library
+const express = require('express');
+
+// define app
+const app = express();
+
+// define port
+const port = 8000;
+
+// run the server
+app.listen(port, function(err){
+    if (err){
+        console.log(`Error in running the server: ${err}`);
+    }
+
+    console.log(`Server is running on port: ${port}`);
+})
